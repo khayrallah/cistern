f64282c9081add525d8e606574f399a6c07ea88c
huda khayrallah <huda@berkeley.edu>
2016-03-21 12:47:40 -0400

    changed print format

---
+++ marmot/src/chipmunk/segmenter/SentenceStemmer.java
@@ -0,0 +1,24 @@
+package chipmunk.segmenter;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Iterator;
+
+import marmot.util.FileUtils;
+
+public class SentenceeStemmer extends Stemmer{
+
+
+
+	public void stemToFile(String outfile, SegmentationDataReader words)
+			throws IOException {
+		Writer writer = FileUtils.openFileWriter(outfile);
+		for (Word word : words) {
+			String stem = stem(word);
+			writer.write(stem);
+			writer.write(' ');
+		}
+		writer.close();
+	}
+
+}
