e06e4c33b95ef7fd04521460076b14da161e0030
Huda Khayrallah <hudak@berkeley.edu>
2016-03-22 21:49:06 -0400

    simplify the code

--- marmot/src/chipmunk/segmenter/SentenceStemmer.java
+++ marmot/src/chipmunk/segmenter/SentenceStemmer.java
@@ -8,16 +8,6 @@ import marmot.util.FileUtils;
 
 public class SentenceeStemmer extends Stemmer{
 
-	public enum FORMAT {
-		WORD, SENTENCE
-	};
-	private Format ;
-
-	public Stemmer(Segmenter segmenter, Mode mode, Format format) {
-		super(segmenter, mode)
-		format_ = format
-	}
-
 	public void stemToFile(String outfile, SegmentationDataReader words)
 			throws IOException {
 		Writer writer = FileUtils.openFileWriter(outfile);
